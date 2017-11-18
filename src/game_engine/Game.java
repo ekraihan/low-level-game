e39c16380356f0a5326d13b4487f135ca6c12c96
ekraihan <ekraihan@iupui.edu>
2017-11-17 22:51:57 -0500

    adding code for keymap

--- src/game_engine/Game.java
+++ src/game_engine/Game.java
@@ -1,24 +1,26 @@
 package game_engine;
 
 import javafx.application.Application;
-import javafx.event.EventHandler;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableMap;
 import javafx.geometry.Dimension2D;
 import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.shape.Circle;
+import javafx.scene.input.KeyCode;
 import javafx.stage.Stage;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Timer;
-import java.util.TimerTask;
+import java.util.*;
 
 abstract public class Game extends Application {
-
     private final static int FRAME_RATE = 30;
+
     private Dimension2D dimensions = new Dimension2D(500, 500);
 
     private List<Sprite> sprite_list = new ArrayList<>();
+
+    private ObservableMap<KeyCode, Boolean> keyMap;
+
+    private Scene scene;
 
     private void update() {
         sprite_list.forEach(Sprite::update);
@@ -36,8 +38,10 @@ abstract public class Game extends Application {
     public void start(Stage stage) throws Exception {
         Group root = new Group();
         sprite_list.forEach(sprite -> root.getChildren().add(sprite));
+        scene = new Scene(root, dimensions.getWidth(), dimensions.getHeight());
 
-        Scene scene = new Scene(root, dimensions.getWidth(), dimensions.getHeight());
+        keyMap = FXCollections.observableMap(new EnumMap<KeyCode, Boolean>(KeyCode.class));
+        Arrays.stream(KeyCode.values()).forEach(value -> keyMap.put(value, false));
 
         stage.setTitle("Low Level Game");
         stage.setScene(scene);
